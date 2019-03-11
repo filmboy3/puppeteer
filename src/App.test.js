8ab47c3674b627cd536eaf770997020e154f5a53
Jonathan Schwartz <filmboy3@yahoo.com>
2019-03-10 23:16:16 -0400

    most info

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const puppeteer = require('puppeteer')
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 50,
+    devtools: true
+  }
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+} 
+
+describe('on page load', () => {
+  test('h1 loads correctly', async (done) => {
+    let browser = await puppeteer.launch(isDebugging())
+    let page = await browser.newPage()
+     
+       page.emulate({
+         viewport: {
+           width: 500,
+           height: 2400
+         },
+         userAgent: ''                                
+       })
+
+       await page.goto('http://localhost:3000/')
+
+       const html = await page.$eval('.App-title', e => e.innerHTML)
+
+       expect(html).toBe('Welcome to React')
+
+       browser.close()
+       done();
+      }, 16000)
+      
+}) 
